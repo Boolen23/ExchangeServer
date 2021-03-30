961bd2905c71d2f0e5f3e0739ddbd20217c1a91b
dis3122 <dis3122@gmail.com>
2021-03-30 23:43:16 +0300

    Добавьте файлы проекта.

---
+++ ExchangeServer/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Net.Sockets;
+
+namespace ExchangeServer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SendMessage(new Random());
+        }
+        private static void SendMessage(Random r)
+        {
+            UdpClient sender = new UdpClient(); // создаем UdpClient для отправки сообщений
+            try
+            {
+                while (true)
+                {
+                    byte[] data = BitConverter.GetBytes(r.Next(10, 20));
+                    sender.EnableBroadcast = true;
+                    sender.Send(data, data.Length, "255.255.255.255", 2222); // отправка
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                sender.Close();
+            }
+        }
+    }
+}
